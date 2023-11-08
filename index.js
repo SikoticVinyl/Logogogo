ac4d4a68c9260d195ac1af8c2145b3170e6ba9b6
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-11-07 19:25:02 -0700

    built out inquirer promtpts

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 const inquirer = require('inquirer');
 const svgGenerator = require('./lib/svgGenerator');
+
+async function main() {
+    try {
+      const userInput = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'text',
+          message: 'Enter text (up to three characters):',
+        },
+        {
+          type: 'input',
+          name: 'textColor',
+          message: 'Enter text color (e.g., blue or #3498db):',
+        },
+        {
+          type: 'list',
+          name: 'shape',
+          message: 'Select a shape:',
+          choices: ['circle', 'triangle', 'square'],
+        },
+        {
+          type: 'input',
+          name: 'shapeColor',
+          message: 'Enter shape color (e.g., green or #2ecc71):',
+        },
+      ]);
+    }
+}
