dfbc460a2d62215fea193cad60c24b80beb350c2
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-11-14 13:55:17 -0700

    updated circle test

--- test/circle.test.js
+++ test/circle.test.js
@@ -1,13 +1,17 @@
-const { Circle } = require('./path-to-your-shape-module');
+const { Circle } = require('../lib/shapes');
 
-describe('Circle Rendering', () => {
-    it('should render the circle correctly', () => {
-        const circle = new Circle();
-        circle.setShapeColor('blue'); // Set the color
+describe('Circle Shape', () => {
+  test('Circle renders correctly', () => {
+    const circle = new Circle();
+    circle.setText('Test');
+    circle.setTextColor('red');
+    circle.setShapeColor('blue');
     
-        const renderedOutput = circle.render();
-
-        expect(renderedOutput).toContain('<circle');
-        expect(renderedOutput).toContain('fill="blue"');
-    });
+    const expectedSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+        <circle cx="100" cy="100" r="100" fill="${this.shapeColor}" />
+        <text x="50%" y="50%" text-anchor="middle" alignment-baseline="middle" font-family="Arial" font-size="30" fill="${this.textColor}">${this.text}</text>
+        </svg>`;
+    
+    expect(circle.render()).toEqual(expectedSVG);
+  });
 });
